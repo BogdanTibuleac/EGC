b5eb4d53a8b6496137be838611d2991decedf936
Bogdan <bogdantibuleac27@gmail.com>
2023-10-24 17:15:41 +0300

    L2

---
+++ tibuleac_bogdan_3131a_l2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tibuleac_bogdan_3131a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Window3D ex = new Window3D())
+            {
+                ex.Run(30.0, 0.0);
+            }
+        }
+    }
+}
