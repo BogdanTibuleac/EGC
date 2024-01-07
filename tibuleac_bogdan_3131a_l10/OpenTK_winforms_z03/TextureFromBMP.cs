7e49e7d3d3eaa840cc95c4dcdf5c1e02edd68ff9
Bogdan <bogdantibuleac27@gmail.com>
2024-01-07 18:38:47 +0200

    tema10

---
+++ tibuleac_bogdan_3131a_l10/OpenTK_winforms_z03/TextureFromBMP.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tibuleac_bogdan_3131a_l10
+{
+    class TextureFromBMP
+    {
+        public int id;
+        public int width;
+        public int height;
+
+        public TextureFromBMP(int _id, int _width, int _height)
+        {
+            id = _id;
+            width = _width;
+            height = _height;
+        }
+    }
+}
